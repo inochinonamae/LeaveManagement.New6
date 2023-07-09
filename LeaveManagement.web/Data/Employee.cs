abb012949e962ef7f5828816f4ec0fe51e80b78c
inochinonamae (Ling Ching Keung) <106677807+inochinonamae@users.noreply.github.com>
2023-07-09 19:14:02 -0400

    Update Layout

---
+++ LeaveManagement.web/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string? Firstname { get; set; } //? Allow to be null
+        public string? Lastname { get; set; }
+        public string? TxtId { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set;}
+    }
+}
---
+++ LeaveManagement.web/Data/LeaveAllocation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema; //Optional, if use Data annotation
+
+namespace LeaveManagement.web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        public int NumberOfDays { get; set; }
+
+
+        //Optional - Data annotation
+        [ForeignKey("LeaveTypeId")]
+
+        //Connect to LeaveType Table
+                // Allows to get details and facilitates inner join much more easily
+        public LeaveType LeaveType { get; set; }
+        // The actual column that represent the foreigh key, right after the connection to the table
+        public int LeaveTypeId { get; set; }
+        
+        //Referencing to EmployeeId
+        public int EmployeeId { get; set; }
+
+    }
+}
